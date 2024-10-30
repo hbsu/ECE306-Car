76823bc0e59ca10ca6dc924d9db5d4246a79d2ca
hbsu <122388917+hbsu@users.noreply.github.com>
2024-10-29 22:38:03 -0400

    Add files via upload

---
+++ hw3/Project 03 cc/Debug/Compiled/subdir_vars.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+OBJ_SRCS += \
+../Compiled/LCD.obj \
+../Compiled/switches.obj \
+../Compiled/system.obj \
+../Compiled/timersB0.obj 
+
+
---
+++ hw3/Project 03 cc/Include/macros.h
@@ -0,0 +1,25 @@
+//===========================================================================
+// File Name : macros.h
+//
+//  Description: This file contains the macros used by all the other files
+//
+//  Author: Henry Su
+//  Date: Sept 2024
+//  Compiler: Built with IAR Embedded Workbench Version: V4.10A/W32 (5.40.1)
+//===========================================================================
+
+
+#define ALWAYS                  (1)
+#define RESET_STATE             (0)
+#define RED_LED              (0x01) // RED LED 0
+#define GRN_LED              (0x40) // GREEN LED 1
+#define TEST_PROBE           (0x01) // 0 TEST PROBE
+#define TRUE                 (0x01) //
+
+
+
+extern volatile unsigned char display_changed;
+extern volatile unsigned char update_display;
+extern volatile unsigned int update_display_count;
+extern volatile unsigned int Time_Sequence;
+extern volatile char one_time;
